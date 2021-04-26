122e08992fd5cf6ec67d72df70244d2db9ef397b
Jean Garcia <jean_clg@hotmail.com>
2021-04-26 16:30:01 -0300

    Create Home list, Gameslist

--- src/components/GamesList.js
+++ src/components/GamesList.js
@@ -1,16 +1,104 @@
-import React, { Component } from 'react'
-import {Link} from "react-router-dom"
-import axios from "axios"
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
-import Navbar from "./Navbar"
-import Footer from "./Footer"
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 export default class GamesList extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
+  state = {
+    games: [],
+    originalList: [],
+    originalCopyList: [],
+    searchGame: "",
+  };
+
+  componentDidMount = async () => {
+    try {
+      const response = await axios.get(
+        `https://www.cheapshark.com/api/1.0/deals?storeID=${this.props.match.params.id}&upperPrice=50`
+      );
+      this.setState({
+        games: [...response.data],
+        originalList: [...response.data],
+        originalCopyList: [...response.data],
+      });
+    } catch (err) {
+      console.error(err);
     }
+  };
+
+  handleChange = (event) => {
+    const newList = this.state.originalCopyList.filter((x) => {
+      return x.title.toLowerCase().includes(event.target.value.toLowerCase());
+    });
+    this.setState({
+      [event.target.name]: event.target.value,
+      originalList: newList,
+    });
+  };
+
+  render() {
+    if (this.state.originalList) {
+      return (
+        <div className="container">
+          <Navbar />
+          <div className="input-group mb-3 mt-3">
+            <input
+              onChange={this.handleChange}
+              type="text"
+              className="form-control"
+              name="searchGame"
+              value={this.state.name}
+              placeholder="Search your game"
+            />
+          </div>
+          <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">SAVINGS</th>
+                <th scope="col">PRICE</th>
+                <th scope="col">TITLE</th>
+                <th scope="col">DEAL RATING</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.originalList.map((x) => {
+                return (
+                  <tr>
+                    <td scope="row">{Math.round(x.savings)}%</td>
+                    <td>
+                      {x.salePrice}
+                      <sup>
+                        <s>{x.normalPrice}</s>
+                      </sup>
+                    </td>
+                    <td>
+                      <img
+                        className="mr-2"
+                        src={x.thumb}
+                        width="120px"
+                        height="45px"
+                      />
+                      {x.title}
+                    </td>
+                    <td>{x.dealRating}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+          <Footer />
+        </div>
+      );
+    } else {
+      return (
+        <div className="d-flex justify-content-center">
+          <div className="spinner-border" role="status">
+            <span className="visually-hidden"></span>
+          </div>
+        </div>
+      );
+    }
+  }
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,42 @@
-import React, { Component } from 'react'
-import {Link} from "react-router-dom"
-import axios from "axios"
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
-
-import Navbar from "./Navbar"
-import Footer from "./Footer"
-
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 export default class Home extends Component {
-    render() {
-        return (
-            <div className="container">
-                <Navbar/>
-                <Footer/>
-            </div>
-        )
+  state = {
+    stores: [],
+  };
+
+  componentDidMount = async () => {
+    try {
+      const response = await axios.get(
+        "https://www.cheapshark.com/api/1.0/stores"
+      );
+      this.setState({ stores: [...response.data] });
+      console.log(this.state.stores);
+    } catch (err) {
+      console.error(err);
     }
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <Navbar />
+        <ul class="list-group">
+          {this.state.stores.map((x) => {
+            return (
+              <Link to={`/GamesList/${x.storeID}`}>
+                <li className="list-group-item">{x.storeName}</li>
+              </Link>
+            );
+          })}
+        </ul>
+        <Footer />
+      </div>
+    );
+  }
 }
