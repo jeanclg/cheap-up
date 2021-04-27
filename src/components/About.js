ea47a231ddc926d31ad30822b5052055229033cd
Felipe Borges <fcborges_@hotmail.com>
2021-04-27 15:35:30 -0300

    About finalizado

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,17 @@
 import React from "react";
 import cheapshark from "./cs_logo.png";
 
+import Linkedin from "./linkedinLogo-removebg-preview.png";
+import Twitter from "./twitter_logo-removebg-preview (1).png";
+import instagram from "./insta_logo-removebg-preview.png";
+import Github from "./Github_logo-removebg-preview.png";
+
 export default function About() {
   return (
-    <div className="container">
+    <div
+      className="container"
+      style={{ textAlign: "center", alignContent: "center" }}
+    >
       <h1
         className="d-flex justify-content-center"
         style={{ color: "#1b1a17" }}
@@ -12,22 +20,25 @@ export default function About() {
       </h1>
 
       <div className="container d-flex flex-column justify-content-center align-items-center">
-        <h4 style={{ color: "#FF8303" }}>
+        <h2 style={{ color: "#FF8303" }}>
           CheapUp is a web application that helps people find the best game
           deals at online platforms.
-        </h4>
-        <h4 style={{ color: "#FF8303" }}>
+        </h2>
+        <h2 style={{ color: "#FF8303" }}>
           Want to have fun and save some money? CheapUp is the way!!
-        </h4>
+        </h2>
+        <br />
         <div className="d-flex flex-column align-items-center">
           <h1 style={{ color: "#1b1a17" }}>
             <i>Tools :</i>
           </h1>
-          <h4 style={{ color: "#FF8303" }}>
+          <h2 style={{ color: "#FF8303" }}>
             CheapUp was made using React.js and an external API as a class
             exercise.
-          </h4>
-          <h4 style={{ color: "#FF8303" }}>We used the CheapShark API,</h4>
+          </h2>
+          <h2 style={{ color: "#FF8303", paddingBottom: "20px" }}>
+            We used the CheapShark API,
+          </h2>
           <a
             target="_blank"
             className="navbar-brand d-flex justify-content-center"
@@ -35,18 +46,101 @@ export default function About() {
           >
             <img
               src={cheapshark}
-              width="90"
-              height="90"
+              width="200"
+              height="200"
               className="d-inline-block align-top"
               alt="logo cheap shark"
             />
           </a>
+          <br />
+          <br />
+          <br />
         </div>
 
-        <div>
-          <h1 style={{ color: "#1b1a17" }}>
-            <i>Creators :</i>
-          </h1>
+        <h1 style={{ color: "#1b1a17" }}>
+          <i>Creators :</i>
+        </h1>
+        <br />
+        <div className="d-flex">
+          <div style={{ paddingRight: "280px" }}>
+            <h3>Felipe Borges</h3>
+            <div className="d-flex align-items-center">
+              <a
+                target="_blank"
+                className="navbar-brand"
+                href="https://github.com/FelipeBorges1991"
+              >
+                <img
+                  src={Github}
+                  width="50"
+                  height="50"
+                  className="d-inline-block align-top"
+                  alt="logo img"
+                />
+              </a>
+
+              <a
+                target="_blank"
+                className="navbar-brand"
+                href="https://github.com/FelipeBorges1991"
+              >
+                <img
+                  src={instagram}
+                  width="50"
+                  height="50"
+                  className="d-inline-block align-top"
+                  alt="logo img"
+                />
+              </a>
+
+              <a
+                target="_blank"
+                className="navbar-brand"
+                href="https://github.com/FelipeBorges1991"
+              >
+                <img
+                  src={Linkedin}
+                  width="50"
+                  height="50"
+                  className="d-inline-block align-top"
+                  alt="logo img"
+                />
+              </a>
+
+              <a
+                target="_blank"
+                className="navbar-brand"
+                href="https://github.com/FelipeBorges1991"
+              >
+                <img
+                  src={Twitter}
+                  width="50"
+                  height="50"
+                  className="d-inline-block align-top"
+                  alt="logo img"
+                />
+              </a>
+            </div>
+          </div>
+
+          <div className="d-flex flex-column align-items-center">
+            <h3>Jean Garcia</h3>
+            <div>
+              <a
+                target="_blank"
+                className="navbar-brand"
+                href="https://github.com/FelipeBorges1991"
+              >
+                <img
+                  src={Github}
+                  width="50"
+                  height="50"
+                  className="d-inline-block align-top"
+                  alt="logo img"
+                />
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
